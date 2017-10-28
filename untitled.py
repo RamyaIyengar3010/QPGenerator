591922ad6f2f68a419d05572753d244355fc202e
RamyaIyengar3010 <ramyapdmnbhn@gmail.com>
2017-10-28 15:03:36 +0530

    Uncommented the program part that added more questions to supply marks deficit Stupid thing still doesnt work, though Need to focus on that part

--- untitled.py
+++ untitled.py
@@ -11,9 +11,13 @@ def generateQP(file, delim):
 		section_num = randrange(len(questions_list))
 		#print section_num, type(section_num)
 		try:
-			question = choice(questions_list[section_num])
+			if questions_list[section_num] is None:
+				questions_list.pop(section_num)
+				marks_list.pop(section_num)
+			else:
+				question = choice(questions_list[section_num])
 		except IndexError:
-			section_numb = 0 if section_numb is len(questions_list) -1 else section_num + 1
+			section_num = 0 if section_num is len(questions_list) -1 else section_num + 1
 		#print question, type(question)
 		sum_marks += marks_list[section_num][1]
 		if sum_marks > 100:
@@ -30,17 +34,25 @@ def generateQP(file, delim):
 	print paperFile
 	print sum_marks
 
-	'''while sum_marks < 100:
+	while sum_marks < 100:
 		remain_marks = 100 - sum_marks
-		for i in marks_list:
-			if remain_marks % i[1] == 0:
-				 print questions_list[i[0]]
-				 question = choice(questions_list[i[0]])
-				 paperFile.append(question)
-				 for n in range(len(questions_list[i[0]])):
-					if questions_list[i[0]][n] is question:
-						questions_list[i[0]].pop(n)
-						break
-	'''
+		for i in reversed(marks_list):
+			try:
+				if questions_list[i[0]] is None:
+					questions_list.pop(i[0])
+					marks_list.pop(i[0])
+				else:
+					if remain_marks % i[1] == 0:
+						 #print questions_list[i[0]]
+						 question = choice(questions_list[i[0]])
+						 paperFile.append(question)
+						 sum_marks += i[1]
+						 for n in range(len(questions_list[i[0]])):
+							if questions_list[i[0]][n] is question:
+								questions_list[i[0]].pop(n)
+								break
+			except IndexError:
+				pass
+	
 
 generateQP("QB1.txt", ['\n', '\r\n'])
